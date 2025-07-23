8d823aee8822558aae4d623a823b9e43857a553b
Rei Calasso <131282394+reicalasso@users.noreply.github.com>
2025-07-23 11:41:21 +0000

    Add WorkspacePage component and update package dependencies

---
+++ pages/workspace.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const WorkspacePage = () => {
+  return (
+    <div>
+      {/* Sayfa içeriğiniz */}
+      Workspace Page
+    </div>
+  );
+};
+
+export default WorkspacePage;
